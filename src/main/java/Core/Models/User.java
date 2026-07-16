246c16d4dd1da95924ec7dd7d07caab86acbd928
Tobi <tobias-kemp@gmx.de>
2026-07-16 17:01:33 +0200

    User, Restaurant und Dish erstellt und Minimal Grundgerüst für User

---
+++ src/main/java/Core/Models/User.java
@@ -0,0 +1,41 @@
+package Core.Models;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class User {
+
+    private final UUID id;
+    private String username;
+    private String email;
+
+    public User(UUID id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
