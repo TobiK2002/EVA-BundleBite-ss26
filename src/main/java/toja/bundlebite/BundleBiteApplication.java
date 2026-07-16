9cb9d863949cd7bba2a8a1e53e3e1c3b5e11b00d
Tobi <tobias-kemp@gmx.de>
2026-07-16 15:09:36 +0200

    Initiales Spring-Boot Setup

---
+++ src/main/java/toja/bundlebite/BundleBiteApplication.java
@@ -0,0 +1,13 @@
+package toja.bundlebite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundleBiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BundleBiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toja/bundlebite/BundleBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package toja.bundlebite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundleBiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
