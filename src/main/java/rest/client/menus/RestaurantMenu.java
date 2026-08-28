6c64accdbc95677da864a582375d743bae6c7524
Tobi <tobias-kemp@gmx.de>
2026-08-28 11:21:42 +0200

    UpdateDish : price von long in double geändert

--- src/main/java/rest/client/menus/RestaurantMenu.java
+++ src/main/java/rest/client/menus/RestaurantMenu.java
@@ -96,7 +96,7 @@ public class RestaurantMenu {
             System.out.print("Beschreibung: ");
             String description = scanner.nextLine();
 
-            System.out.print("Preis (Format: [Euro].[Cent]): ");
+            System.out.print("Preis in Euro.Cent: ");
             double price = Double.parseDouble(scanner.nextLine());
 
             System.out.print("Zutaten kommagetrennt: ");
@@ -138,8 +138,8 @@ public class RestaurantMenu {
             System.out.print("Beschreibung: ");
             String description = scanner.nextLine();
 
-            System.out.print("Preis in Cent: ");
-            long price = Long.parseLong(scanner.nextLine());
+            System.out.print("Preis in Euro.Cent: ");
+            double price = Double.parseDouble(scanner.nextLine());
 
             System.out.print("Zutaten kommagetrennt: ");
             List<String> ingredients = List.of(scanner.nextLine().split(","));
